8cf3983fa839793b98da3a4f68354d7eb3c386d4
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-02-12 19:10:47 +0530

    edit function added

--- server.js
+++ server.js
@@ -103,8 +103,3 @@ app.delete("/api/todos/:id", (req, res) => {
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
