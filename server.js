2ffba19afce1c4e060846051786d86f9e9da174e
VaishnaviGund42 <vaishnavigund42@gmail.com>
2026-02-12 19:09:57 +0530

    edit function added

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 const FILE_PATH = "./data.json";
+
+app.get("/", (req, res) => {
+  res.send("Todo Backend is Running 🚀");
+});
 
 
 // Read Todos
@@ -94,6 +98,10 @@ app.delete("/api/todos/:id", (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Failed to delete todo" });
   }
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 
